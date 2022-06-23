96d12309b596d0b076242a1719bcdc3a864803be
Dario Ceballos <dario_tec_aut@hotmail.com.ar>
2022-06-23 09:09:29 -0300

    Dia 2

--- usuarioCelularConsusltas.sql
+++ usuarioCelularConsusltas.sql
@@ -1,3 +1,5 @@
+
+--DIA 1
 --Listar los nombres de los usuarios
 select nombre from Usuarios ;
 
@@ -17,6 +19,20 @@ where compania = 'AT&T';
 select email from Usuarios 
 where email like '%hotmail%';
 
+-- DIA 2
+--Calcular el saldo máximo de los usuarios de sexo “Mujer”
+select max(saldo),sexo from Usuarios
+where sexo = 'M';
+--Listar nombre y teléfono de los usuarios con teléfono LG, SAMSUNG o MOTOROLA
+select nombre,telefono from Usuarios
+where marca = 'LG' or marca = 'SAMSUNG' or marca = 'MOTOROLA';
+--Listar las diferentes compañías en orden alfabético aleatorio
+select DISTINCT(marca) from Usuarios;
+--Listar las diferentes compañías en orden alfabético descendentemente
+select DISTINCT(marca) from Usuarios order by marca desc;
+--Listar los nombres de los usuarios sin saldo o inactivos
+select nombre from Usuarios 
+where saldo<=0 or activo = false;
 
 
 
--- usuarioProvinciaConsulta.sql
+++ usuarioProvinciaConsulta.sql
@@ -3,3 +3,7 @@ SELECT provincias.nombre FROM provincias
 left join clientes
 on clientes.codigoProvincia = provincias.codigo
 where clientes.codigoProvincia is null;
+
+--¿Qué provincias tienen clientes? Pero sin repetir el nombre de la provincia. Un tip, vas a necesitar la sentencia distinct
+select distinct(provincias.nombre) from provincias 
+inner join clientes on clientes.codigoProvincia = provincias.codigo;
